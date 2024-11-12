9434e20bcfaa2b4cc93e3012b2d170915bdf5d20
Daniel <danielanderson.ti@gmail.com>
2024-11-12 00:42:31 -0300

    Segundo Commit.Comentarios a classe ConverterCamelCase

--- Downloads/ADS/Java Coursera/QuebraStringsCamelCase/src/com/quebraStrings/ConverterCamelCase.java
+++ Downloads/ADS/Java Coursera/QuebraStringsCamelCase/src/com/quebraStrings/ConverterCamelCase.java
@@ -5,9 +5,17 @@ import java.util.ArrayList;
 
 public class ConverterCamelCase {
     
+    // Método estático que recebe uma string 'original' como parâmetro.
+    // O objetivo do método é converter a string para minúsculas e armazená-la em uma lista.
     public static List<String> converterCamelCase(String original) {
+        
+        // Criação de uma lista para armazenar os resultados.
         List<String> resultado = new ArrayList<>();
+        
+        // Adiciona a versão da string 'original' convertida para minúsculas na lista 'resultado'.
         resultado.add(original.toLowerCase());
+        
+        // Retorna a lista contendo a string convertida para minúsculas.
         return resultado;
     }
 }
