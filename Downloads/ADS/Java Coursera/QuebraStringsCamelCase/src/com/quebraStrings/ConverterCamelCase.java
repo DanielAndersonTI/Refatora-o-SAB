45812f5b4f9e36ee0182cbee320e7e02d4aca74b
Daniel <danielanderson.ti@gmail.com>
2024-11-12 00:39:25 -0300

    Primeiro Commit

---
+++ Downloads/ADS/Java Coursera/QuebraStringsCamelCase/src/com/quebraStrings/ConverterCamelCase.java
@@ -0,0 +1,21 @@
+package com.quebraStrings;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ConverterCamelCase {
+    
+    public static List<String> converterCamelCase(String original) {
+        List<String> resultado = new ArrayList<>();
+        resultado.add(original.toLowerCase());
+        return resultado;
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Downloads/ADS/Java Coursera/QuebraStringsCamelCase/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module QuebraStringsCamelCase {
+    requires org.junit.jupiter.api;
+    requires org.junit.jupiter.engine;  
+    requires org.junit.platform.launcher;  
+    }
